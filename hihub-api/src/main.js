a4703e6be8a329e6aaaed97267db9de4f99e1cc7
Dreamice <dreamice13@foxmail.com>
2022-11-20 10:50:25 +0800

    API：项目添加路由

--- hihub-api/src/main.js
+++ hihub-api/src/main.js
@@ -1,10 +1,10 @@
 const Koa = require('koa')
-const { APP_PORT } = require('./config.default')
+const { APP_PORT } = require('./config/config.default')
+const userRouter = require('./router/user.route')
 
 const app = new Koa()
-app.use((ctx, next) => {
-    ctx.body = 'hello node'
-})
+
+app.use(userRouter.routes())
 
 app.listen(APP_PORT, () => {
     console.log(`HiHub-API server is running on port ${APP_PORT}`);
---
+++ hihub-api/src/router/user.route.js
@@ -0,0 +1,8 @@
+const Router = require('koa-router')
+
+const router = new Router({ prefix: '/users' })
+router.get('/', (ctx, next) => {
+    ctx.body = 'hello users'
+})
+
+module.exports = router
