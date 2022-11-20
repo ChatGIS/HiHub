79185daa3b6304e49068ca5418d5b0cd856fbd91
Dreamice <dreamice13@foxmail.com>
2022-11-20 11:45:42 +0800

    API：项目模块化，拆分服务，业务，路由和控制器层

--- hihub-api/src/main.js
+++ hihub-api/src/main.js
@@ -1,10 +1,5 @@
-const Koa = require('koa')
 const { APP_PORT } = require('./config/config.default')
-const userRouter = require('./router/user.route')
-
-const app = new Koa()
-
-app.use(userRouter.routes())
+const app = require('./app/index')
 
 app.listen(APP_PORT, () => {
     console.log(`HiHub-API server is running on port ${APP_PORT}`);
