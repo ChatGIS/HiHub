7419d1ea7938ac566c411272258477cb21601646
Dreamice <dreamice13@foxmail.com>
2022-11-21 23:04:51 +0800

    API：路由自动加载

--- hihub-api/src/app/index.js
+++ hihub-api/src/app/index.js
@@ -1,14 +1,11 @@
 const Koa = require('koa')
 const { koaBody } = require('koa-body');
 
-const userRouter = require('../router/user.route')
-const tagRouter = require('../router/tag.route')
+const router = require('../router')
 
 const app = new Koa()
 
 app.use(koaBody())
-
-app.use(userRouter.routes())
-app.use(tagRouter.routes())
+app.use(router.routes()).use(router.allowedMethods())
 
 module.exports = app
