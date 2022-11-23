9956935795603e783e93c83df8bf672260a1eba5
Dreamice <dreamice13@foxmail.com>
2022-11-23 21:13:07 +0800

    API：增加允许跨域koa2-cors

--- hihub-api/src/app/index.js
+++ hihub-api/src/app/index.js
@@ -1,11 +1,13 @@
 const Koa = require('koa')
 const { koaBody } = require('koa-body');
+cors = require('koa2-cors')
 
 const router = require('../router')
 const response = require('../middleware/response')
 
 const app = new Koa()
 
+app.use(cors())
 app.use(koaBody())
 app.use(response())
 app.use(router.routes()).use(router.allowedMethods())
