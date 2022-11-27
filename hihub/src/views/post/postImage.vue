97b1e77ff950016bc67dc67c436c2c014c4fb758
Dreamice <dreamice13@foxmail.com>
2022-11-27 09:14:33 +0800

    图片展示，添加弹出框实现点击放大图片

--- hihub/src/views/post/postImage.vue
+++ hihub/src/views/post/postImage.vue
@@ -1,5 +1,6 @@
 <script setup lang='ts'>
 import { getImagesByPostId } from '@/api/post'
+import { getTagOfPost } from '@/api/tag'
 import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -26,7 +27,13 @@ const initImages = async () => {
     const res = await getImagesByPostId(queryParam)
     images.push(...res.images)
 }
+// 初始化标签
+const initTags = async () => {
+    const res = await getTagOfPost(queryParam)
+    console.log(res)
+}
 initImages()
+initTags()
 
 // 改变图片布局函数
 const changeImageLayout = () => {
@@ -41,17 +48,21 @@ const changeImageLayout = () => {
                 <el-affix :offset="200">
                     <h3>{{ postName }}</h3>
                     <div style="margin: 0 0 15px 0">
-                    <el-icon @click="changeImageLayout" :size="25">
-                        <Grid v-show="!isShowGrid" />
-                        <Iphone v-show="isShowGrid" />
-                    </el-icon>
-                </div>
+                        <el-icon @click="changeImageLayout" :size="25">
+                            <Grid v-show="!isShowGrid" />
+                            <Iphone v-show="isShowGrid" />
+                        </el-icon>
+                    </div>
                 </el-affix>
             </el-col>
             <el-col :span="18">
                 <el-image v-for="item in images" :key="item.id" :src="item.url" fit="contain" v-show="!isShowGrid" />
-                <el-image v-for="item in images" :key="item.id" :src="item.url" fit="fill" v-show="isShowGrid"
-                    style="width: 20%" />
+                <el-popover placement="right" :width="600" trigger="click" v-for="item in images" :key="item.id">
+                    <template #reference>
+                        <el-image :src="item.url" fit="fill" v-show="isShowGrid" style="width: 20%" />
+                    </template>
+                    <el-image :src="item.url" fit="contain" />
+                </el-popover>
             </el-col>
         </el-row>
     </div>
